038c38c1a4e7578afb2a5dfac08ba319237ca991
yangxin12138 <995249880@qq.com>
2023-11-22 14:58:47 +0800

    2023.11.21:添加恢复出厂菜单

--- app/src/main/java/com/twd/setting/module/device/DeviceActivity.java
+++ app/src/main/java/com/twd/setting/module/device/DeviceActivity.java
@@ -1,7 +1,11 @@
 package com.twd.setting.module.device;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,10 +22,14 @@ public class DeviceActivity extends AppCompatActivity implements View.OnClickLis
     private final static String TAG = DeviceActivity.class.getName();
     private LinearLayout LL_info;
     private LinearLayout LL_storage;
+    private LinearLayout LL_factory;
     private TextView tv_info;
     private TextView tv_storage;
+    private TextView tv_factory;
     private ImageView arrow_info;
     private ImageView arrow_storage;
+    private ImageView arrow_factory;
+    private Context context;
     String theme_code = SystemPropertiesUtils.getPropertyColor("persist.sys.background_blue","0");
 
     @Override
@@ -39,6 +47,7 @@ public class DeviceActivity extends AppCompatActivity implements View.OnClickLis
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
+        context = this;
     }
 
     @Override
@@ -50,13 +59,17 @@ public class DeviceActivity extends AppCompatActivity implements View.OnClickLis
     private void initView(){
         LL_info = findViewById(R.id.devices_LL_Info);
         LL_storage = findViewById(R.id.devices_LL_storage);
+        LL_factory = findViewById(R.id.devices_LL_factory);
         tv_info = findViewById(R.id.devices_tv_Info);
         tv_storage = findViewById(R.id.devices_tv_storage);
+        tv_factory = findViewById(R.id.devices_tv_factory);
         arrow_info = findViewById(R.id.arrow_info);
         arrow_storage = findViewById(R.id.arrow_storage);
+        arrow_factory = findViewById(R.id.arrow_factory);
 
         LL_info.setOnClickListener(this::onClick);
         LL_storage.setOnClickListener(this::onClick);
+        LL_factory.setOnClickListener(this::onClick);
 
         LL_info.requestFocus();
     }
@@ -67,9 +80,42 @@ public class DeviceActivity extends AppCompatActivity implements View.OnClickLis
         if(view.getId() == R.id.devices_LL_Info){
             intent = new Intent(this,DeviceInfoActivity.class);
             startActivity(intent);
-        }else {
+        }else if (view.getId() == R.id.devices_LL_storage){
             intent = new Intent(this,DeviceStorageActivity.class);
             startActivity(intent);
+        }else {
+            showDialog();
         }
+    }
+
+    private void showDialog(){
+        Dialog FactoryDialog = new Dialog(this,R.style.DialogStyle);
+
+        //加载自定义布局文件
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.factory_dialog, null);
+        FactoryDialog.setContentView(dialogView);
+        dialogView.setPadding(100,0,100,50);
+
+        final TextView factoryTitle = dialogView.findViewById(R.id.factory_title);
+        final LinearLayout okBT = dialogView.findViewById(R.id.factory_ok_bt);
+        final LinearLayout cancelBT = dialogView.findViewById(R.id.factory_cancel_bt);
+        factoryTitle.setText("恢复出厂设置会重启");
+        FactoryDialog.show();
+
+        okBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: factory ok");
+            }
+        });
+
+        cancelBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: factory cancel");
+                FactoryDialog.dismiss();
+            }
+        });
     }
 }
