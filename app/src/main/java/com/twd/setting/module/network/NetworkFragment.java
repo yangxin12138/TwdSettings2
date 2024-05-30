1e23565272c33e1585c08bf5d266cb0e5a0ff2c4
Mulgokizary-ever <45618055+yangxin12138@users.noreply.github.com>
2024-05-30 16:00:27 +0800

    2024.5.30：#allwinner 1：网络列表在一级菜单

--- app/src/main/java/com/twd/setting/module/network/NetworkFragment.java
+++ app/src/main/java/com/twd/setting/module/network/NetworkFragment.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Paint;
 import android.net.NetworkInfo;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
@@ -16,6 +17,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -57,12 +59,25 @@ import java.util.List;
 import java.util.Set;
 public class NetworkFragment
         extends BaseFragment
-        implements  ConnectivityListener.Listener{
+        implements  ConnectivityListener.Listener, WifiListRvAdapter.IWifiItemClickListener
+        ,WifiAccessPoint.AccessPointListener, ConnectivityListener.WifiNetworkListener{
     private final String TAG = "NetworkFragment";
     private FragmentNetworkBinding binding;
     private ConnectivityListener mConnectivityListener;
 
+    public static String selectedBSSID;
+    public static String selectedSSID;
     private WifiManager wifiManager;
+    private List<WifiAccessPoint> wifiAccessPoints;
+    private WifiListRvAdapter adapter;
+    private long mNoWifiUpdateBeforeMillis;
+    private final Handler mHandler = new Handler();
+    private final Runnable mInitialUpdateWifiListRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateWifiList();
+        }
+    };
     public static NetworkFragment newInstance() {
         return new NetworkFragment();
     }
@@ -70,9 +85,12 @@ public class NetworkFragment
     private void updateWifi() {
         if (mConnectivityListener.isWifiEnabledOrEnabling()) {
             binding.switchWifi.setChecked(true);
+            updateWifiList();
             return;
         }
         binding.switchWifi.setChecked(false);
+        binding.networkWifiList.setVisibility(binding.switchWifi.isChecked()?View.VISIBLE:View.INVISIBLE);
+        Log.i(TAG, "updateWifi: networklist = "+binding.networkWifiList.getVisibility()+",switch = "+binding.switchWifi.isChecked());
     }
 
 
@@ -80,6 +98,14 @@ public class NetworkFragment
 
     @Override
     public void onConnectivityChange() {
+        updateConnectivity();
+    }
+
+    private void updateConnectivity(){
+        if (mConnectivityListener.isWifiEnabledOrEnabling()){
+            Log.i(TAG, "updateConnectivity: ------resume-----");
+            updateWifi();
+        }
     }
 
     public void onCreate(Bundle paramBundle) {
@@ -94,9 +120,75 @@ public class NetworkFragment
         wifiManager = ((WifiManager)requireContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE));
     }
 
+    private void updateWifiList(){
+        Log.i(TAG, "updateWifiList: ");
+        if (!mConnectivityListener.isWifiEnabledOrEnabling()){
+            Log.i(TAG, "updateWifiList: 网络没打开");
+            wifiAccessPoints.clear();
+            adapter.clearAll();
+            mNoWifiUpdateBeforeMillis = 0L;
+            return;
+        }
+        wifiAccessPoints.clear();
+        adapter.clearAll();
+        long l = SystemClock.elapsedRealtime();
+        Log.d(TAG, "updateWifiList  2222  l:"+l+", mNoWifiUpdateBeforeMillis:"+mNoWifiUpdateBeforeMillis);
+        if (mNoWifiUpdateBeforeMillis > l){
+            mHandler.removeCallbacks(mInitialUpdateWifiListRunnable);
+            mHandler.postDelayed(mInitialUpdateWifiListRunnable,mNoWifiUpdateBeforeMillis -l);
+            return;
+        }
+        Log.d(TAG, "updateWifiList  3333");
+        int j = wifiAccessPoints.size();
+        HashSet localHashSet = new HashSet(j);
+        int i = 0;
+        while ( i < j){
+            localHashSet.add((WifiAccessPoint) wifiAccessPoints.get(i));
+            i += 1;
+        }
+
+        List<WifiAccessPoint> wifiAccessPoint_list = mConnectivityListener.getAvailableNetworks();
+        Log.d(TAG, "updateWifiList: wifiAccessPoint_list is " + wifiAccessPoint_list.isEmpty());
+        wifiAccessPoints.clear();
+        Iterator iterator = wifiAccessPoint_list.iterator();
+        while (iterator.hasNext()){
+            WifiAccessPoint wifiAccessPoint = (WifiAccessPoint) iterator.next();
+            wifiAccessPoint.setListener(this);
+            if (wifiAccessPoint.getTag() == null){
+                wifiAccessPoint.setTag(wifiAccessPoint);
+            }else {
+                localHashSet.remove((WifiAccessPoint) wifiAccessPoint.getTag());
+            }
+            Log.d(TAG,"list:"+wifiAccessPoint.getSsid()+",state:"+((wifiAccessPoint.getNetworkInfo()==null)?"null": wifiAccessPoint.getNetworkInfo().getState()));
+            wifiAccessPoints.add(wifiAccessPoint);
+        }
+        Iterator iterator_hashset = localHashSet.iterator();
+        while (iterator_hashset.hasNext()){
+            wifiAccessPoints.remove(iterator_hashset.next());
+        }
+
+        if ((wifiAccessPoints != null) && (wifiAccessPoints.size() != 0)){
+            adapter.notifyWifiAccessPoints();
+        }
+    }
+
     public View onCreateView(LayoutInflater paramLayoutInflater, ViewGroup paramViewGroup, Bundle paramBundle) {
         binding = (FragmentNetworkBinding) DataBindingUtil.inflate(paramLayoutInflater, R.layout.fragment_network, paramViewGroup, false);
+        initWifiList();
         return binding.getRoot();
+    }
+
+    private void initWifiList(){
+        wifiAccessPoints = new ArrayList<WifiAccessPoint>();
+        binding.networkWifiList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new WifiListRvAdapter();
+        adapter.setWifiAccessPoints(wifiAccessPoints);
+        adapter.setItemClickListener(this);
+        adapter.notifyWifiAccessPoints();
+        binding.networkWifiList.setItemAnimator(null);
+        binding.networkWifiList.setAdapter(adapter);
+        binding.networkWifiList.addItemDecoration(new MarginTopItemDecoration(getResources().getDimensionPixelSize(R.dimen.net_rc_item_margin_t)));
     }
 
     public void onDestroy() {
@@ -117,6 +209,7 @@ public class NetworkFragment
     @Override
     public void onStart() {
         super.onStart();
+        mNoWifiUpdateBeforeMillis = (SystemClock.elapsedRealtime() + 500L);
     }
     @Override
     public void onViewCreated(View paramView, Bundle paramBundle) {
@@ -136,14 +229,17 @@ public class NetworkFragment
                 if(binding.switchWifi.isChecked()){
                     wifiManager.setWifiEnabled(false);
                     binding.itemWifiAvailable.setVisibility(View.GONE);
+                    binding.networkWifiList.setVisibility(View.INVISIBLE);
                 }else {
                     wifiManager.setWifiEnabled(true);
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
                             binding.itemWifiAvailable.setVisibility(View.VISIBLE);
+                            binding.networkWifiList.setVisibility(View.VISIBLE);
+                            updateWifiList();
                         }
-                    },1000);
+                    },4000);
                 }
             }
         });
@@ -183,4 +279,46 @@ public class NetworkFragment
             }
         }
     };
+
+    @Override
+    public void onFocusRequest(View paramView, int paramInt) {
+
+    }
+
+    @Override
+    public void onItemClick(WifiAccessPoint paramWifiAccessPoint) {
+        if (paramWifiAccessPoint == null){
+            selectedSSID = "Other";
+            selectedBSSID = "add a new network";
+            startActivity(new Intent(getContext(), AddWifiNetworkActivity.class));
+        }else {
+            selectedSSID = paramWifiAccessPoint.getSsidStr();
+            selectedBSSID = paramWifiAccessPoint.getBssid();
+            startActivity(WifiConnectionActivity.createIntent(getContext(),paramWifiAccessPoint));
+        }
+    }
+
+    @Override
+    public void onAccessPointChanged(WifiAccessPoint wifiAccessPoint) {
+        WifiAccessPoint accessPoint = (WifiAccessPoint) wifiAccessPoint.getTag();
+        if (accessPoint != null){
+            int position = adapter.getWifiAccessPoints().indexOf(accessPoint);
+            Log.d(TAG,"onAccessPointChanged  position = "+position+", WifiAccessPoint"+accessPoint);
+            adapter.notifyItemChanged(position,accessPoint);
+        }
+    }
+
+    @Override
+    public void onLevelChanged(WifiAccessPoint paramWifiAccessPoint) {
+        paramWifiAccessPoint = (WifiAccessPoint) paramWifiAccessPoint.getTag();
+        if (paramWifiAccessPoint !=null){
+            WifiListRvAdapter localWifiListRvAdapter = adapter;
+            localWifiListRvAdapter.notifyItemChanged(localWifiListRvAdapter.getWifiAccessPoints().indexOf(paramWifiAccessPoint),paramWifiAccessPoint);
+        }
+    }
+
+    @Override
+    public void onWifiListChanged() {
+        updateWifiList();
+    }
 }
