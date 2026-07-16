9e2fd40fdfcea1fc62423256fe61a72d7331034d
yangxin <995249880@qq.com>
2026-07-16 14:30:51 +0800

    2026.07.16:#mt6582:修改投影首页对外接收调用，能直接打开四点梯形矫正

--- app/src/main/java/com/twd/setting/module/projector/ProjectorActivity.java
+++ app/src/main/java/com/twd/setting/module/projector/ProjectorActivity.java
@@ -9,13 +9,29 @@ import androidx.fragment.app.FragmentManager;
 import com.twd.setting.R;
 import com.twd.setting.base.BaseActivity;
 import com.twd.setting.module.projector.fragment.ProjectorFragment;
+import com.twd.setting.module.projector.fragment.SinglePointFragment;
 import com.twd.setting.utils.SystemPropertiesUtils;
 import com.twd.setting.utils.UiUtils;
 
 public class ProjectorActivity extends BaseActivity {
+    public static final String EXTRA_TARGET_FRAGMENT = "target_fragment";
+
+    public static final int FRAG_PROJECTOR = 0;
+    public static final int FRAG_SINGLE_POINT = 1;
+
+
     protected void onCreate(Bundle paramBundle){
         super.onCreate(paramBundle);
-        UiUtils.replaceFragment(getSupportFragmentManager(),16908290, ProjectorFragment.newInstance());
+        int targetFragment = getIntent().getIntExtra(EXTRA_TARGET_FRAGMENT, FRAG_PROJECTOR);
+        Fragment target;
+        if (targetFragment == FRAG_SINGLE_POINT) {
+            // 外部请求：打开单点梯形校正 SinglePointFragment
+            target = SinglePointFragment.newInstance();
+        } else {
+            // 默认页面
+            target = ProjectorFragment.newInstance();
+        }
+        UiUtils.replaceFragment(getSupportFragmentManager(), 16908290, target);
     }
 
     FragmentManager fragmentManager = getSupportFragmentManager();
